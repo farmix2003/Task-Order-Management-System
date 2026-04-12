e7ad030996c5011fd8e6a8d346e598c322411a4e
Farrukh <ffarrux386@gmail.com>
2026-04-12 07:04:46 -0700

    initial structure

---
+++ src/main/java/farmix/com/FarmixApplication.java
@@ -0,0 +1,13 @@
+package farmix.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/farmix/com/FarmixApplicationTests.java
@@ -0,0 +1,13 @@
+package farmix.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
